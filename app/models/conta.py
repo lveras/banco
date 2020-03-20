fa36c87e03e4b3a3804f99ec000cfdd728c273c7
Luciano Veras <luciano.veras@hotmail.com>
2020-03-20 12:19:35 -0300

    [REF] Nome das tabelas direto no template

--- app/models/conta.py
+++ app/models/conta.py
@@ -1,9 +1,11 @@
 from sqlalchemy import Column, Integer
 from sqlalchemy.orm import relationship
 
-from app.db.base_class import Base
+from app.core.database import Base
 
 
 class Conta(Base):
+    __tablename__ = 'contas'
     id = Column(Integer, primary_key=True, index=True)
+
     movimentacoes = relationship("Movimentacao", back_populates="conta")
--- app/models/movimentacao.py
+++ app/models/movimentacao.py
@@ -1,12 +1,13 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship
 
-from app.db.base_class import Base
+from app.core.database import Base
 
 
 class Movimentacao(Base):
+    __tablename__ = 'movimentacoes'
     id = Column(Integer, primary_key=True, index=True)
     tipo = Column(String, index=True)
     valor = Column(Float, index=True)
-    conta_id = Column(Integer, ForeignKey("conta.id"))
+    conta_id = Column(Integer, ForeignKey("contas.id"))
     conta = relationship("Conta", back_populates="movimentacoes")
