8d55ca16224889f628d4776915d1987ae2d3ec6a
Luciano Veras <luciano.veras@hotmail.com>
2020-03-20 15:22:07 -0300

    [ADD] Validações na rota de deposito

--- app/api/api_v1/endpoints/movimentacao.py
+++ app/api/api_v1/endpoints/movimentacao.py
@@ -1,12 +1,21 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app import crud
 from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.schemas import movimentacao
+from typing import List
+
 router = APIRouter()
 
+TIPOS_MOVIMENTACAO = ['saque', 'deposito', 'transferencia']
 
-@router.post("/", response_model=conta.Conta)
+
+@router.post("/", response_model=List[movimentacao.Movimentacao])
 def create_deposito(*, db: Session = Depends(get_db),
-                    movimentacao_in: conta.ContaCreate):
-    return crud.conta.create(db_session=db, obj_in=movimentacao_in)
+                    movimentacao_in: movimentacao.MovimentacaoCreate):
+    conta = crud.conta.get(db_session=db, id=movimentacao_in.conta_id)
+    if not conta:
+        raise HTTPException(status_code=422, detail="Conta não encontrada")
+    if movimentacao_in.tipo not in TIPOS_MOVIMENTACAO:
+        raise HTTPException(status_code=422, detail="Tipo inválido")
+    return crud.movimentacao.create(db_session=db, obj_in=movimentacao_in)
