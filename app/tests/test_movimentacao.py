2cce6e65c80b31bbf31e5dea0e0436df4089499d
Luciano Veras <luciano.veras@hotmail.com>
2020-03-20 14:07:45 -0300

    [REF] Criação do usuário pelo crud

--- app/tests/test_movimentacao.py
+++ app/tests/test_movimentacao.py
@@ -1,12 +1,18 @@
 from fastapi.testclient import TestClient
+from fastapi import Depends
 from app.main import app
+from app import crud
+from app.core.database import SessionLocal
+from app.schemas.conta import ContaCreate
 
 client = TestClient(app)
 
 
-def test_cria_deposito():
-    c = client.post(url="/api/v1/conta/", json={})
-    id_conta = c.json()['id']
-    j = {'id': id_conta, 'tipo': 'deposito', 'valor': 100.0}
-    response = client.post(url="/api/v1/deposito/", json=j)
+def test_cria_movimentacao_deposito():
+    conta_in = ContaCreate()
+    conta = crud.conta.create(db_session=SessionLocal(), obj_in=conta_in)
+    j = {'conta_id': conta.id, 'tipo': 'deposito', 'valor': 100.0}
+    response = client.post(url="/api/v1/movimentacao/", json=j)
     assert response.status_code == 200
+
+    crud.conta.remove(db_session=SessionLocal(), id=conta.id)
