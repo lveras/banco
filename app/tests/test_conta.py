db1047562700d7c5c88309d64ae113f7b13e8f11
Luciano Veras <luciano.veras@hotmail.com>
2020-03-20 12:07:24 -0300

    [ADD] Teste para listar conta

--- app/tests/test_conta.py
+++ app/tests/test_conta.py
@@ -2,10 +2,21 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
+ID_CONTA = ''
 
 
 def test_cria_conta():
-    response = client.post("/api/v1/conta/", json={})
+    response = client.post(url="/api/v1/conta/", json={})
+    data = response.json()
+    assert response.status_code == 200
+    assert 'id' in data
+    assert 'movimentacoes' in data
+    assert type(data['movimentacoes']) is list
+
+
+def test_lista_conta_pelo_id():
+    c = client.post(url="/api/v1/conta/", json={})
+    response = client.get(url="/api/v1/conta/{}".format(c.json()['id']))
     data = response.json()
     assert response.status_code == 200
     assert 'id' in data
