3b8b536af873e7352364eaad386bf987e4900b65
Luciano Veras <luciano.veras@hotmail.com>
2020-03-20 15:18:52 -0300

    [ADD] Rota para movimentacao

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from app.api.api_v1.endpoints import conta
+from app.api.api_v1.endpoints import conta, movimentacao
 
 api_router = APIRouter()
 api_router.include_router(conta.router, prefix="/conta", tags=["conta"])
+api_router.include_router(
+    movimentacao.router, prefix="/movimentacao", tags=["movimentacao"])
