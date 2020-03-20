1f024f3015dd6248d598f5c5875230bc94b51cc3
Luciano Veras <luciano.veras@hotmail.com>
2020-03-20 12:09:35 -0300

    [ADD] Rota para endpoint conta

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -2,4 +2,4 @@ from fastapi import APIRouter
 from app.api.api_v1.endpoints import conta
 
 api_router = APIRouter()
-api_router.include_router(conta.router, tags=["conta"])
+api_router.include_router(conta.router, prefix="/conta", tags=["conta"])
