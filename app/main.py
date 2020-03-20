0f3dd3c3c73c3befe96b9efcc44f07be362ef584
Luciano Veras <luciano.veras@hotmail.com>
2020-03-20 12:21:35 -0300

    [ADD] Ignora database

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+import uvicorn
 from app.api.api_v1.api import api_router
 from app.core import config
+from app.models import conta, movimentacao
+from app.core.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(title=config.PROJECT_NAME, openapi_url="/api/v1/openapi.json")
 
 app.include_router(api_router, prefix=config.API_V1_STR)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
