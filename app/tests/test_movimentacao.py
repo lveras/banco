5e44726bb05cd82a5ceeef60451f0c887756d5bf
Luciano Veras <luciano.veras@hotmail.com>
2020-03-20 15:17:49 -0300

    [ADD] Verifica se esta aplicando a taxa bancária

--- app/tests/test_movimentacao.py
+++ app/tests/test_movimentacao.py
@@ -4,7 +4,6 @@ from app.main import app
 from app import crud
 from app.core.database import SessionLocal
 from app.schemas.conta import ContaCreate
-
 client = TestClient(app)
 
 
@@ -13,7 +12,10 @@ def test_cria_movimentacao_deposito():
     conta = crud.conta.create(db_session=SessionLocal(), obj_in=conta_in)
     j = {'conta_id': conta.id, 'tipo': 'deposito', 'valor': 100.0}
     response = client.post(url="/api/v1/movimentacao/", json=j)
+    res = response.json()
     assert response.status_code == 200
-    assert j['tipo'] == response.json()['tipo']
-    assert j['valor'] == response.json()['valor']
+    assert res[0]['tipo'] == j['tipo']
+    assert res[0]['valor'] == 99
+    assert res[1]['tipo'] == 'taxa'
+    assert res[1]['valor'] == 1
     crud.conta.remove(db_session=SessionLocal(), id=conta.id)
