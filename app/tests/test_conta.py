44eae85401319a1d49e076178195da9b909ab815
Luciano Veras <luciano.veras@hotmail.com>
2020-03-22 13:12:44 -0300

    [FIX] retirado decorador(não funcionando)

--- app/tests/test_conta.py
+++ app/tests/test_conta.py
@@ -1,8 +1,8 @@
 from fastapi.testclient import TestClient
 from app.main import app
-from app.tests.test_movimentacao import conta_fake
 client = TestClient(app)
 ID_CONTA = ''
+from app.tests.test_movimentacao import conta_fake
 
 
 def test_cria_conta():
@@ -14,17 +14,16 @@ def test_cria_conta():
     assert type(data['movimentacoes']) is list
 
 
-@conta_fake
-def test_pega_extrato_da_conta(conta_id):
+def test_pega_extrato_da_conta(conta_id=conta_fake()):
     j = {'conta_id': conta_id, 'tipo': 'deposito', 'valor': 100.0}
     client.post(url="/api/v1/movimentacao/deposito", json=j)
     j = {'conta_id': conta_id, 'tipo': 'deposito', 'valor': 200.0}
     client.post(url="/api/v1/movimentacao/deposito", json=j)
     response = client.get(
         url="/api/v1/conta/extrato/{}".format(conta_id))
-
     data = response.json()
     assert response.status_code == 200
     assert 'id' in data
     assert 'movimentacoes' in data
     assert type(data['movimentacoes']) is list
+    conta_fake(conta_id=conta_id)
--- app/tests/test_movimentacao.py
+++ app/tests/test_movimentacao.py
@@ -8,34 +8,30 @@ from app.schemas.conta import ContaCreate
 client = TestClient(app)
 
 
-def conta_fake(func):
-    def log():
-        def wrapped():
-            conta_in = ContaCreate()
-            conta = crud.conta.create(
-                db_session=SessionLocal(), obj_in=conta_in)
-            func(conta.id)
-            crud.conta.remove(db_session=SessionLocal(), id=conta.id)
-        return wrapped
-    return log
+def conta_fake(conta_id=False):
+    if not conta_id:
+        conta_in = ContaCreate()
+        conta = crud.conta.create(
+            db_session=SessionLocal(), obj_in=conta_in)
+        return conta.id
+    crud.conta.remove(db_session=SessionLocal(), id=conta_id)
 
 
-@conta_fake
-def test_cria_movimentacao_deposito(conta_id):
+def test_cria_movimentacao_deposito(conta_id=conta_fake()):
     j = {'conta_id': conta_id, 'valor': 100.0}
     response = client.post(url="/api/v1/movimentacao/deposito", json=j)
     res = response.json()
     assert response.status_code == 200
-    assert res[0]['tipo'] == j['tipo']
+    assert res[0]['tipo'] == 'deposito'
     assert res[0]['valor'] == 100
     assert res[0]['saldo_conta_atual'] == 100
     assert res[1]['tipo'] == 'taxa'
-    assert res[1]['valor'] == 1
+    assert res[1]['valor'] == -1
     assert res[1]['saldo_conta_atual'] == 99
+    conta_fake(conta_id)
 
 
-@conta_fake
-def test_cria_movimentacao_saque(conta_id):
+def test_cria_movimentacao_saque(conta_id=conta_fake()):
     j = {'conta_id': conta_id, 'valor': 200.0}
     client.post(url="/api/v1/movimentacao/deposito", json=j)
     j = {'conta_id': conta_id, 'tipo': 'saque', 'valor': 100.0}
@@ -48,3 +44,4 @@ def test_cria_movimentacao_saque(conta_id):
     assert res[1]['tipo'] == 'taxa'
     assert res[1]['valor'] == -4
     assert res[1]['saldo_atual'] == 94
+    conta_fake(conta_id=conta_id)
