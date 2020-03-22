2028cce3d0926d2e38093cf2ee401533b25f7f1e
Luciano Veras <luciano.veras@hotmail.com>
2020-03-22 12:41:14 -0300

    [REF] alterado rota para extrato

--- app/api/api_v1/endpoints/conta.py
+++ app/api/api_v1/endpoints/conta.py
@@ -6,13 +6,13 @@ from app.schemas import conta
 router = APIRouter()
 
 
-@router.get("/{id}", response_model=conta.Conta)
-def ver_conta(id: int, db: Session = Depends(get_db)):
-    conta = crud.conta.get(db_session=db, id=id)
-    return conta
-
-
 @router.post("/", response_model=conta.Conta)
 def create_conta(*, db: Session = Depends(get_db),
                  conta_in: conta.ContaCreate):
     return crud.conta.create(db_session=db, obj_in=conta_in)
+
+
+@router.get("/extrato/{id}", response_model=conta.ContaExtrato)
+def ver_extrato_conta(id: int, db: Session = Depends(get_db)):
+    conta = crud.conta.get(db_session=db, id=id)
+    return conta
