241180c820d9700b8bf956e04f3154af7dbce078
Luciano Veras <luciano.veras@hotmail.com>
2020-03-22 14:44:18 -0300

    [REF] Refeito testes contendo nova estrutura e adicionado 2 testes para saldo insuficiente e conta não encontrada

--- app/tests/test_movimentacao.py
+++ app/tests/test_movimentacao.py
@@ -17,6 +17,14 @@ def conta_fake(conta_id=False):
     crud.conta.remove(db_session=SessionLocal(), id=conta_id)
 
 
+def test_cria_movimentacao_conta_nao_encontrada():
+    j = {'conta_id': 0, 'valor': 100.0}
+    response = client.post(url="/api/v1/movimentacao/deposito", json=j)
+    res = response.json()
+    assert response.status_code == 422
+    assert res['detail'] == 'Conta nao encontrada'
+
+
 def test_cria_movimentacao_deposito(conta_id=conta_fake()):
     j = {'conta_id': conta_id, 'valor': 100.0}
     response = client.post(url="/api/v1/movimentacao/deposito", json=j)
@@ -24,10 +32,9 @@ def test_cria_movimentacao_deposito(conta_id=conta_fake()):
     assert response.status_code == 200
     assert res[0]['tipo'] == 'deposito'
     assert res[0]['valor'] == 100
-    assert res[0]['saldo_conta_atual'] == 100
-    assert res[1]['tipo'] == 'taxa'
-    assert res[1]['valor'] == -1
-    assert res[1]['saldo_conta_atual'] == 99
+    assert res[0]['taxa'] == -1
+    assert res[0]['saldo_movimentacao'] == 99
+    assert res[0]['saldo_conta_atual'] == 99
     conta_fake(conta_id)
 
 
@@ -39,9 +46,19 @@ def test_cria_movimentacao_saque(conta_id=conta_fake()):
     res = response.json()
     assert response.status_code == 200
     assert res[0]['tipo'] == j['tipo']
-    assert res[0]['valor'] == -100
-    assert res[0]['saldo_conta_atual'] == 98
-    assert res[1]['tipo'] == 'taxa'
-    assert res[1]['valor'] == -4
-    assert res[1]['saldo_conta_atual'] == 94
+    assert res[0]['valor'] == 100
+    assert res[0]['taxa'] == -4
+    assert res[0]['saldo_movimentacao'] == -96
+    assert res[0]['saldo_conta_atual'] == 102
+    conta_fake(conta_id=conta_id)
+
+
+def test_cria_movimentacao_saque_saldo_insuficiente(conta_id=conta_fake()):
+    j = {'conta_id': conta_id, 'valor': 200.0}
+    client.post(url="/api/v1/movimentacao/deposito", json=j)
+    j = {'conta_id': conta_id, 'tipo': 'saque', 'valor': 200.0}
+    response = client.post(url="/api/v1/movimentacao/saque", json=j)
+    res = response.json()
+    assert response.status_code == 422
+    assert res['detail'] == 'Saldo insuficiente'
     conta_fake(conta_id=conta_id)
